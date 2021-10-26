04a75ae3abd05dd9dd86beff75b066a7f690e724
Artem Deykun <artemhp@gmail.com>
2021-10-26 19:51:26 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import Grid from './Grid';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Grid cols={10} rows={10} tick={100} />
+    <Grid cols={50} rows={15} tick={100} />
   </React.StrictMode>,
   document.getElementById('root')
 );
